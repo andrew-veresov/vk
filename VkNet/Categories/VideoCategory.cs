5e3414cffdbc3ff21281666f75ad6832db134023
Ефремов Алексей <main.efremov@gmail.com>
2015-05-07 12:06:30 +0300

    В категорию Video добавлены новые функции.
    Добавлены функции вида GetAll* и SearchAll*.

--- VkNet/Categories/VideoCategory.cs
+++ VkNet/Categories/VideoCategory.cs
@@ -25,6 +25,41 @@
         }
 
         /// <summary>
+        /// Возвращает информацию о всех видеозаписях.
+        /// </summary>
+        /// <param name="ownerId">
+        /// Идентификатор пользователя или сообщества, которому принадлежат видеозаписи.
+        /// Обратите внимание, идентификатор сообщества в параметре <paramref name="ownerId"/> необходимо указывать со знаком "-" — например, 
+        /// <paramref name="ownerId"/>=-1 соответствует идентификатору сообщества ВКонтакте API (club1).
+        /// </param>
+        /// <param name="albumId">Идентификатор альбома, видеозаписи из которого нужно вернуть.</param>
+        /// <param name="width">Требуемая ширина изображений видеозаписей в пикселах.</param>
+        /// <param name="extended">Определяет, возвращать ли информацию о настройках приватности видео для текущего пользователя.</param>
+        /// <returns>После успешного выполнения возвращает список объектов видеозаписей с дополнительным полем comments, содержащим число комментариев у 
+        /// видеозаписи. Если был задан параметр <paramref name="extended"/>, то для каждой видеозаписи возвращаются дополнительные поля: 
+        /// <see cref="Video.CanComment"/>, <see cref="Video.CanRepost"/>, <see cref="Video.Likes"/></returns>
+        /// <remarks>
+        /// Для вызова этого метода Ваше приложение должно иметь права с битовой маской, содержащей <see cref="Settings.Video"/>.
+        /// Страница документации ВКонтакте <see href="http://vk.com/dev/video.get"/>.
+        /// </remarks>
+        [Pure]
+        [ApiVersion("5.9")]
+        public ReadOnlyCollection<Video> GetAll(long? ownerId = null, long? albumId = null, VideoWidth width = VideoWidth.Medium160, bool extended = false)
+        {
+            const int count = 200;
+            var i = 0;
+            var result = new List<Video>();
+
+            do
+            {
+                var currentItems = Get(ownerId, albumId, width, count, i * count, extended);
+                if (currentItems != null) result.AddRange(currentItems);
+            } while (++i * count < (_vk.CountFromLastResponse ?? 0));
+
+            return result.ToReadOnlyCollection();
+        }
+
+        /// <summary>
         /// Возвращает информацию о видеозаписях.
         /// </summary>
         /// <param name="ownerId">
@@ -252,6 +287,37 @@
         }
 
         /// <summary>
+        /// Возвращает список всех видеозаписей в соответствии с заданным критерием поиска.
+        /// </summary>
+        /// <param name="query">Строка поискового запроса.</param>
+        /// <param name="sort">Вид сортировки.</param>
+        /// <param name="isHd">Если true, то поиск производится только по видеозаписям высокого качества.</param>
+        /// <param name="isAdult">Фильтр «Безопасный поиск».</param>
+        /// <param name="filters">Список критериев, по которым требуется отфильтровать видео.</param>
+        /// <param name="isSearchOwn">Искать по видеозаписям пользователя.</param>
+        /// <returns>После успешного выполнения возвращает список объектов видеозаписей.</returns>
+        /// <remarks>
+        /// Для вызова этого метода Ваше приложение должно иметь права с битовой маской, содержащей <see cref="Settings.Video"/>.
+        /// Страница документации ВКонтакте <see href="http://vk.com/dev/video.search"/>.
+        /// </remarks>
+        [Pure]
+        [ApiVersion("5.9")]
+        public ReadOnlyCollection<Video> SearchAll(string query, VideoSort sort, bool isHd = false, bool isAdult = false, VideoFilters filters = null, bool isSearchOwn = false)
+        {
+            const int count = 200;
+            var i = 0;
+            var result = new List<Video>();
+
+            do
+            {
+                var currentItems = Search(query, sort, isHd, isAdult, filters, isSearchOwn, count, i * count);
+                if (currentItems != null) result.AddRange(currentItems);
+            } while ((++i * count < (_vk.CountFromLastResponse ?? 0)) && (i * count < 1000));
+
+            return result.ToReadOnlyCollection();
+        }
+
+        /// <summary>
         /// Возвращает список видеозаписей в соответствии с заданным критерием поиска.
         /// </summary>
         /// <param name="query">Строка поискового запроса.</param>
@@ -295,6 +361,33 @@
         }
 
         /// <summary>
+        /// Возвращает список всех видеозаписей, на которых отмечен пользователь.
+        /// </summary>
+        /// <param name="userId">Идентификатор пользователя.</param>
+        /// <returns>После успешного выполнения возвращает список объектов видеозаписей.</returns>
+        /// <remarks>
+        /// ЭТОТ МЕТОД ВЫБРАСЫВАЕТ ИСКЛЮЧЕНИЕ НА СЕРВЕРЕ ВК!!!
+        /// Для вызова этого метода Ваше приложение должно иметь права с битовой маской, содержащей <see cref="Settings.Video"/>.
+        /// Страница документации ВКонтакте <see href="http://vk.com/dev/video.getUserVideos"/>.
+        /// </remarks>
+        [Pure]
+        [ApiVersion("5.9")]
+        public ReadOnlyCollection<Video> GetAllUserVideos(long userId)
+        {
+            const int count = 100;
+            var i = 0;
+            var result = new List<Video>();
+
+            do
+            {
+                var currentItems = GetUserVideos(userId, count, i * count);
+                if (currentItems != null) result.AddRange(currentItems);
+            } while (++i * count < (_vk.CountFromLastResponse ?? 0));
+
+            return result.ToReadOnlyCollection();
+        }
+
+        /// <summary>
         /// Возвращает список видеозаписей, на которых отмечен пользователь.
         /// </summary>
         /// <param name="userId">Идентификатор пользователя.</param>
@@ -326,6 +419,38 @@
             VkResponseArray response = _vk.Call("video.getUserVideos", parameters);
 
             return response.ToReadOnlyCollectionOf<Video>(x => x);
+        }
+
+        /// <summary>
+        /// Возвращает список всех альбомов видеозаписей пользователя или сообщества.
+        /// </summary>
+        /// <param name="ownerId">Идентификатор владельца альбомов (пользователь или сообщество). По умолчанию — идентификатор текущего 
+        /// пользователя.</param>
+        /// <param name="extended">true – позволяет получать поля <see cref="VideoAlbum.Count"/>, <see cref="VideoAlbum.Photo160"/> и 
+        /// <see cref="VideoAlbum.Photo320"/> для каждого альбома.</param>
+        /// <returns>
+        /// После успешного выполнения возвращает массив объектов <see cref="VideoAlbum"/>, каждый из которых содержит следующие 
+        /// поля: <see cref="VideoAlbum.Id"/>, <see cref="VideoAlbum.OwnerId"/> и <see cref="VideoAlbum.Title"/>.
+        /// </returns>
+        /// <remarks>
+        /// Для вызова этого метода Ваше приложение должно иметь права с битовой маской, содержащей <see cref="Settings.Video"/>.
+        /// Страница документации ВКонтакте <see href="http://vk.com/dev/video.getAlbums"/>.
+        /// </remarks>
+        [Pure]
+        [ApiVersion("5.9")]
+        public ReadOnlyCollection<VideoAlbum> GetAllAlbums(long ownerId, bool extended = false)
+        {
+            const int count = 100;
+            var i = 0;
+            var result = new List<VideoAlbum>();
+
+            do
+            {
+                var currentItems = GetAlbums(ownerId, count, i * count, extended);
+                if (currentItems != null) result.AddRange(currentItems);
+            } while (++i * count < (_vk.CountFromLastResponse ?? 0));
+
+            return result.ToReadOnlyCollection();
         }
 
         /// <summary>
@@ -470,6 +595,39 @@
             parameters.Add("video_ids", videoIds);
             
             return _vk.Call("video.moveToAlbum", parameters);
+        }
+
+        /// <summary>
+        /// Возвращает список всех комментариев к видеозаписи.
+        /// </summary>
+        /// <param name="videoId">Идентификатор видеозаписи.</param>
+        /// <param name="ownerId">Идентификатор пользователя или сообщества, которому принадлежит видеозапись.
+        /// Обратите внимание, идентификатор сообщества в параметре <paramref name="ownerId"/> необходимо указывать со знаком "-" — например, 
+        /// <paramref name="ownerId"/>=-1 соответствует идентификатору сообщества ВКонтакте API (club1).
+        /// </param>
+        /// <param name="needLikes">true — будет возвращено дополнительное поле <see cref="Comment.Likes"/>. По умолчанию поле <see cref="Comment.Likes"/> 
+        /// не возвращается.</param>
+        /// <param name="sort">Порядок сортировки комментариев.</param>
+        /// <returns>После успешного выполнения возвращает общее количество комментариев и массив объектов <see cref="Comment"/>.</returns>
+        /// <remarks>
+        /// Для вызова этого метода Ваше приложение должно иметь права с битовой маской, содержащей <see cref="Settings.Video"/>.
+        /// Страница документации ВКонтакте <see href="http://vk.com/dev/video.getComments"/>.
+        /// </remarks>
+        [Pure]
+        [ApiVersion("5.9")]
+        public ReadOnlyCollection<Comment> GetAllComments(long videoId, long? ownerId = null, bool needLikes = false, CommentsSort sort = null)
+        {
+            const int count = 100;
+            var i = 0;
+            var result = new List<Comment>();
+
+            do
+            {
+                var currentItems = GetComments(videoId, ownerId, needLikes, count, i * count, sort);
+                if (currentItems != null) result.AddRange(currentItems);
+            } while (++i * count < (_vk.CountFromLastResponse ?? 0));
+
+            return result.ToReadOnlyCollection();
         }
 
         /// <summary>
@@ -738,7 +896,34 @@
 
             return _vk.Call("video.removeTag", parameters);
         }
-        
+
+        /// <summary>
+        /// Возвращает список всех видеозаписей, на которых есть непросмотренные отметки. 
+        /// </summary>
+        /// <returns>
+        /// После успешного выполнения возвращает список объектов <see cref="Video"/> с дополнительным полем <see cref="Video.Tag"/>.
+        /// </returns>
+        /// <remarks>
+        /// Для вызова этого метода Ваше приложение должно иметь права с битовой маской, содержащей <see cref="Settings.Video"/>.
+        /// Страница документации ВКонтакте <see href="http://vk.com/dev/video.getNewTags"/>.
+        /// </remarks>        
+        [Pure]
+        [ApiVersion("5.9")]
+        public ReadOnlyCollection<Video> GetAllNewTags()
+        {
+            const int count = 100;
+            var i = 0;
+            var allVideos = new List<Video>();
+
+            do
+            {
+                var currentVideos = GetNewTags(count, i * count);
+                if (currentVideos != null) allVideos.AddRange(currentVideos);
+            } while (++i * count < (_vk.CountFromLastResponse ?? 0));
+
+            return allVideos.ToReadOnlyCollection();
+        }
+
         /// <summary>
         /// Возвращает список видеозаписей, на которых есть непросмотренные отметки. 
         /// </summary>
